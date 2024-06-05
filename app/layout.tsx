3498dd9f18bf8a47c1b71cf6a7b74a04205479d9
Mishaj__k <amstig100@gmail.com>
2024-06-05 18:34:17 +0530

    feat(header): add UI with framer-motion animations
    - Implemented header component with animated UI elements using framer-motion.
    - Included smooth transition effects for a better user experience.
    - Ensured responsive design for different screen sizes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body className={`${inter.className} bg-slate-50`}>
         <div className="bg-[#dcffd3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#cad6ff] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        {children}</body>
+        <Header/>
+        {children}
+      </body>
     </html>
   );
 }
